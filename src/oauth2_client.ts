fa56b70339b151e7cc96c095b984ea84f6ad1103
Jonas Chen <jonaschen@Jonass-MacBook-Pro.local>
2021-10-05 23:42:06 +0800

    Azure

--- src/oauth2_client.ts
+++ src/oauth2_client.ts
@@ -9,16 +9,39 @@ export interface OAuth2ClientConfig {
   clientSecret?: string;
   /** The URI of the client's redirection endpoint (sometimes also called callback URI). */
   redirectUri?: string;
+  /** Must include code for the authorization code flow. 
+   * Can also include id_token or token if using the hybrid flow.*/
+  responseType?: string;
 
   /** The URI of the authorization server's authorization endpoint. */
   authorizationEndpointUri: string;
+
   /** The URI of the authorization server's token endpoint. */
   tokenUri: string;
-  
-  /** To enable applications to use incremental authorization to request access to additional scopes in context. 
-  If you set this parameter's value to true and the authorization request is granted, 
-  then the new access token will also cover any scopes to which the user previously granted the application access. */
-  includeGrantedScopes?: boolean;
+
+  /** To enable applications to use incremental authorization to request access to additional scopes in context.
+   * If you set this parameter's value to true and the authorization request is granted, 
+   * then the new access token will also cover any scopes to which the user previously granted the application access. */
+  includeGrantedScopes?: string;
+
+  /** The {tenant} value in the path of the request can be used to control who can sign into the application. 
+   * The allowed values are common, organizations, consumers, and tenant identifiers. */
+  tenant?: string;
+
+  /** Used to secure authorization code grants via Proof Key for Code Exchange (PKCE).
+   * Required if code_challenge_method is included. For more information, see the PKCE RFC. */
+  codeChallenge?: string;
+
+  /** Used to secure authorization code grants via Proof Key for Code Exchange (PKCE). 
+   * Required if code_challenge_method is included. For more information, see the PKCE RFC. */
+  codeChallengeMethod?: string;
+
+  /** Specifies the method that should be used to send the resulting token back to your app.
+   * "query" provides the code as a query string parameter on your redirect URI. 
+   * If you're requesting an ID token using the implicit flow, you can't use query as specified in the OpenID spec.
+   * If you're requesting just the code, you can use query, "fragment", or "form_post". 
+   * "form_post" executes a POST containing the code to your redirect URI. */
+  responseMode?: string;
 
   defaults?: {
     /**
@@ -30,7 +53,7 @@ export interface OAuth2ClientConfig {
     /** Default scopes to request unless otherwise specified. */
     scope?: string | string[];
     /** Default state validator to use for validating the authorization response's state value. */
-    stateValidator?: (state: string | null) => boolean;
+    stateValidator?: (state: string | null) => boolean;  
   };
 }
 
