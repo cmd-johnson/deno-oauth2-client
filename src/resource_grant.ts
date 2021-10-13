222af820ef29dd261086f4768cb48cae51dc86d8
lovshains <jonas@lovshains.com>
2021-10-13 22:32:23 +0800

    Update host feature

--- src/resource_grant.ts
+++ src/resource_grant.ts
@@ -17,7 +17,9 @@ export class ResourceGrant extends OAuth2GrantBase {
             "Authorization": `Bearer ${token}`,
             "content-type": "application/json",
         };
-        const resourceUrl: string = this.client.config.resourceEndpointHost + resourcePath
+        const resourceUrl: string = this.client.config.resourceEndpointHost.startWith('http') ? 
+            this.client.config.resourceEndpointHost : 
+            this.client.config.resourceEndpointHost + resourcePath
         const request = this.buildRequest(resourceUrl, {
             method,
             headers
